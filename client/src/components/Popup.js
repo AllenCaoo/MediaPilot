4c130e7107917ac167811529e8b56162356da171
Annabella Chow <annabella.chow@berkeley.edu>
2023-10-28 17:48:56 -0700

    width popup

--- client/src/components/Popup.js
+++ client/src/components/Popup.js
@@ -1,17 +1,32 @@
-import React, {useState} from 'react';
+// import React, {useState} from 'react';
 
+// import Button from '@mui/material/Button';
+// import Dialog from '@mui/material/Dialog';
+// import DialogActions from '@mui/material/DialogActions';
+// import DialogContent from '@mui/material/DialogContent';
+// import DialogTitle from '@mui/material/DialogTitle';
+
+
+import React, {useState} from 'react';
+import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
-
-
+import FormControl from '@mui/material/FormControl';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import Select from '@mui/material/Select';
+import Switch from '@mui/material/Switch';
 
 export function Popup() {
 
   const [open, setOpen] = useState(true);
-
+  const [fullWidth, setFullWidth] = React.useState(true);
+  const [maxWidth, setMaxWidth] = React.useState('sm');
   const handleOpen = () => {
     setOpen(true);
   };
@@ -19,9 +34,14 @@ export function Popup() {
   const handleClose = () => {
     setOpen(false);
   };
+
+
   function Modal({ open, onClose, title, content, actions }) {
     return (
-      <Dialog open={open} onClose={onClose}>
+      <Dialog open={open} 
+              onClose={onClose} 
+              fullWidth={'xl'}
+              maxWidth={'xl'}>
         <DialogTitle>{title}</DialogTitle>
         <DialogContent>{content}</DialogContent>
         <DialogActions>{actions}</DialogActions>
@@ -49,6 +69,10 @@ export function Popup() {
 }
 
 export default Popup
+
+
+
+
 
 
 
